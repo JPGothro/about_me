1baaeedd026b53d49d9549b3a0072c07fc3028e8
TomTimmel <tom.t.timmel@gmail.com>
2016-08-18 13:57:09 -0700

    paired update to John about_me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # about_me
 Stuff about me
 - Getting ready to commit the initial code.
+- Tom is the driver extrodinaire, and John is the Not Netscape Navigator!
+-We worked on the code like a couple of bosses...
--- app.js
+++ app.js
@@ -14,83 +14,98 @@ var answerArray = new Array();
 var answerCount = 0;
 
 // On to the questions and answers, including checking the validity...
-currentQuestion = 'Was John born in the Pacific Northwest?';
-answer = prompt(currentQuestion).toLowerCase();
-questionArray.push(currentQuestion);
-answerArray.push(answer);
+function birthQuestion() {
+  currentQuestion = 'Was John born in the Pacific Northwest?';
+  answer = prompt(currentQuestion).toLowerCase();
+  questionArray.push(currentQuestion);
+  answerArray.push(answer);
 
-if (answer === 'y' || answer === 'yes') {
-  // it's a YES!
-  alert('You got that right! He was born in Washington state.');
-  answerCount++;
-} else if (answer === 'n' || answer === 'no') {
-  // that is a NO.
-  alert('Sorry, that is not the right answer: he actually was born in the PacNW.');
-} else {
-  // the answer was crap!
-  alert('Now, did you not read that answers could only be Yes, No, Y or N? Illiterate much? Or simply contrary?');
+  if (answer === 'y' || answer === 'yes') {
+    // it's a YES!
+    alert('You got that right! He was born in Washington state.');
+    answerCount++;
+  } else if (answer === 'n' || answer === 'no') {
+    // that is a NO.
+    alert('Sorry, that is not the right answer: he actually was born in the PacNW.');
+  } else {
+    // the answer was crap!
+    alert('Now, did you not read that answers could only be Yes, No, Y or N? Illiterate much? Or simply contrary?');
+  }
+  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
 }
-console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+birthQuestion();
 
-currentQuestion = 'Does John have a valid passport?';
-answer = prompt(currentQuestion).toLowerCase();
-questionArray.push(currentQuestion);
-answerArray.push(answer);
+function passportQuestion() {
+  currentQuestion = 'Does John have a valid passport?';
+  answer = prompt(currentQuestion).toLowerCase();
+  questionArray.push(currentQuestion);
+  answerArray.push(answer);
 
-if (answer === 'y' || answer === 'yes') {
-  alert('Yeppers, John has a valid passport! He can leave the country. Hm...');
-  answerCount++;
-} else if (answer === 'n' || answer === 'no') {
-  alert('No, no, no! He does have a valid one!');
-} else {
-  alert('Stop with the bad input, already!');
+  if (answer === 'y' || answer === 'yes') {
+    alert('Yeppers, John has a valid passport! He can leave the country. Hm...');
+    answerCount++;
+  } else if (answer === 'n' || answer === 'no') {
+    alert('No, no, no! He does have a valid one!');
+  } else {
+    alert('Stop with the bad input, already!');
+  }
+  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
 }
-console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+passportQuestion();
 
-currentQuestion = 'Has John ever had a broken bone?';
-answer = prompt(currentQuestion).toLowerCase();
-questionArray.push(currentQuestion);
-answerArray.push(answer);
+function boneQuestion() {
+  currentQuestion = 'Has John ever had a broken bone?';
+  answer = prompt(currentQuestion).toLowerCase();
+  questionArray.push(currentQuestion);
+  answerArray.push(answer);
 
-if (answer === 'y' || answer === 'yes') {
-  alert('You answered YES, which is wrong-a-mundo!');
-} else if (answer === 'n' || answer === 'no') {
-  alert('No, John has been lucky enough to never have had a broken bone.');
-  answerCount++;
-} else {
-  alert('What?! Crap input? Please!!');
+  if (answer === 'y' || answer === 'yes') {
+    alert('You answered YES, which is wrong-a-mundo!');
+  } else if (answer === 'n' || answer === 'no') {
+    alert('No, John has been lucky enough to never have had a broken bone.');
+    answerCount++;
+  } else {
+    alert('What?! Crap input? Please!!');
+  }
+  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
 }
-console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+boneQuestion();
 
-currentQuestion = 'Has he had any viable off-spring?';
-answer = prompt(currentQuestion).toLowerCase();
-questionArray.push(currentQuestion);
-answerArray.push(answer);
+function viableQuestion() {
+  currentQuestion = 'Has he had any viable off-spring?';
+  answer = prompt(currentQuestion).toLowerCase();
+  questionArray.push(currentQuestion);
+  answerArray.push(answer);
 
-if (answer === 'y' || answer === 'yes') {
-  alert('Yeah, no. John does not have any off-spring. Unless those aliens have done something nefarious...');
-} else if (answer === 'n' || answer === 'no') {
-  alert('No children for John. He\'s enough of a child on his own. Correct!');
-  answerCount++;
-} else {
-  alert('Bad input is just childish!');
+  if (answer === 'y' || answer === 'yes') {
+    alert('Yeah, no. John does not have any off-spring. Unless those aliens have done something nefarious...');
+  } else if (answer === 'n' || answer === 'no') {
+    alert('No children for John. He\'s enough of a child on his own. Correct!');
+    answerCount++;
+  } else {
+    alert('Bad input is just childish!');
+  }
+  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
 }
-console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+viableQuestion();
 
-currentQuestion = 'Is John a friend-of-dogs?';
-answer = prompt(currentQuestion).toLowerCase();
-questionArray.push(currentQuestion);
-answerArray.push(answer);
+function dogQuestion() {
+  currentQuestion = 'Is John a friend-of-dogs?';
+  answer = prompt(currentQuestion).toLowerCase();
+  questionArray.push(currentQuestion);
+  answerArray.push(answer);
 
-if (answer === 'y' || answer === 'yes') {
-  alert('Yes, he is a friend to dogs. At least friendly ones.');
-  answerCount++;
-} else if (answer === 'n' || answer === 'no') {
-  alert('Just what do you think of John?!!? He is shocked!');
-} else {
-  alert('Invalid input is for the dogs.');
+  if (answer === 'y' || answer === 'yes') {
+    alert('Yes, he is a friend to dogs. At least friendly ones.');
+    answerCount++;
+  } else if (answer === 'n' || answer === 'no') {
+    alert('Just what do you think of John?!!? He is shocked!');
+  } else {
+    alert('Invalid input is for the dogs.');
+  }
+  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
 }
-console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+dogQuestion();
 
 console.log('There were ' + answerCount + ' correct answers, out of 5 total.');
 
@@ -99,62 +114,69 @@ for (var i = 0; i < questionArray.length; i++) {
 };
 
 // Adding a number guessing game.
-alert('Welcome to my next guessing game.');
-alert('An integer has been chosen at random from 1 to 10. You have 4 chances to guess it.');
+function numberGuessingGame() {
+  alert('Welcome to my next guessing game.');
+  alert('An integer has been chosen at random from 1 to 10. You have 4 chances to guess it.');
 
-var randomNum = Math.floor(Math.random() * 10) + 1;
-console.log('the random number is:', randomNum);
+  var randomNum = Math.floor(Math.random() * 10) + 1;
+  console.log('the random number is:', randomNum);
 
-var gotIt = false;
-var userGuess = '';
-for (var j = 0 ; j < 4 ; j++ ){
-  userGuess = prompt('Please, guess a number from 1 to 10');
-  if (userGuess === NaN) {
-    alert('Numbers only, please!');
-  }
-  var userGuessNum = parseInt(userGuess);
-  if (randomNum === userGuessNum) {
-    alert('You got it!');
-    // do not count every time they got it right....
-    if (!gotIt) {
-      answerCount++;
-      gotIt = true;
+  var gotIt = false;
+  var userGuess = '';
+  for (var j = 0 ; j < 4 ; j++ ){
+    userGuess = prompt('Please, guess a number from 1 to 10');
+    if (userGuess === NaN) {
+      alert('Numbers only, please!');
     }
-    console.log('Guessed the right number on try #', j + 1);
-  } else if (randomNum > userGuessNum) {
-    alert('Your guess was too low. Try again.');
-  } else {
-    alert('That guess was too high. Try again.');
+    var userGuessNum = parseInt(userGuess);
+    if (randomNum === userGuessNum) {
+      alert('You got it!');
+      // do not count every time they got it right....
+      if (!gotIt) {
+        answerCount++;
+        gotIt = true;
+      }
+      console.log('Guessed the right number on try #', j + 1);
+    } else if (randomNum > userGuessNum) {
+      alert('Your guess was too low. Try again.');
+    } else {
+      alert('That guess was too high. Try again.');
+    }
   }
 }
+numberGuessingGame();
 
 // Adding another guessing game.
-alert('Welcome to another guessing game.');
-alert('Aside from Oregon, guess a state I have lived in. You have up to 6 chances to hit one.');
+function stateGuessingGame() {
+  alert('Welcome to another guessing game.');
+  alert('Aside from Oregon, guess a state I have lived in. You have up to 6 chances to hit one.');
 
-var statesVisitedArray = ['New Jersey', 'Ohio', 'Washington'];
-console.log('the possible state answers are:', statesVisitedArray.join(', '));
-var stateGuessed = false;
-var stateCount = 1;
+  var statesVisitedArray = ['New Jersey', 'Ohio', 'Washington'];
+  console.log('the possible state answers are:', statesVisitedArray.join(', '));
+  var stateGuessed = false;
+  var stateCount = 1;
+  var userGuess = '';
 
-while (stateCount < 7 && !stateGuessed) {
-  userGuess = prompt('Your guess, please: spelling counts!');
-  userGuess = userGuess.toLowerCase();
-  for (var k = 0; (k < statesVisitedArray.length && !stateGuessed); k++){
-    if (userGuess === statesVisitedArray[k].toLowerCase()) {
-      stateGuessed = true;
-      alert('Yes, that is one of them. They are all: ' + statesVisitedArray.join(', '));
-      answerCount++;
+  while (stateCount < 7 && !stateGuessed) {
+    userGuess = prompt('Your guess, please: spelling counts!');
+    userGuess = userGuess.toLowerCase();
+    for (var k = 0; (k < statesVisitedArray.length && !stateGuessed); k++){
+      if (userGuess === statesVisitedArray[k].toLowerCase()) {
+        stateGuessed = true;
+        alert('Yes, that is one of them. They are all: ' + statesVisitedArray.join(', '));
+        answerCount++;
+      }
+    }
+    if (!stateGuessed) {
+      stateCount++;
     }
   }
+
   if (!stateGuessed) {
-    stateCount++;
+    alert('Sorry you did not guess one. They are all: ' + statesVisitedArray.join(', '));
   }
 }
-
-if (!stateGuessed) {
-  alert('Sorry you did not guess one. They are all: ' + statesVisitedArray.join(', '));
-}
+stateGuessingGame();
 
 // Let the user know how they did on all that guessing.
 switch (answerCount){
