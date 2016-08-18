172007674da1c3d4542ac7c0cf1c98df62959da2
TomTimmel <tom.t.timmel@gmail.com>
2016-08-18 16:37:15 -0700

    2nd stretch goal

--- app.js
+++ app.js
@@ -16,101 +16,126 @@ var answerCount = 0;
 var theQuestions = ['Was John born in the Pacific Northwest?', 'Does John have a valid passport?', 'Has John ever had a broken bone?', 'Has he had any viable off-spring?', 'Is John a friend-of-dogs?'];
 var correctAnswers = ['yes', 'yes', 'no', 'no','yes'];
 var incorrectAnswers = ['no', 'no', 'yes', 'yes', 'no'];
+var correctMessages = ['You got that right! He was born in Washington state.', 'Yeppers, John has a valid passport! He can leave the country. Hm...', 'No, John has been lucky enough to never have had a broken bone.', 'No children for John. He\'s enough of a child on his own. Correct!', 'Yes, he is a friend to dogs. At least friendly ones.'];
+
+for (var k = 0; k < theQuestions.length; k++) {
+  basicQuestion(k);
+}
 
 // On to the questions and answers, including checking the validity...
-function birthQuestion() {
-  currentQuestion = theQuestions[0];
+function basicQuestion(setId) {
+  currentQuestion = theQuestions[setId];
   answer = prompt(currentQuestion).toLowerCase();
   questionArray.push(currentQuestion);
   answerArray.push(answer);
 
-  if (answer === correctAnswers[0] || answer === correctAnswers[0][0]) {
+  if (answer === correctAnswers[setId] || answer === correctAnswers[setId][0]) {
     // it's a YES!
-    alert('You got that right! He was born in Washington state.');
+    alert(correctMessages[setId]);
     answerCount++;
-  } else if (answer === incorrectAnswers[0] || answer === incorrectAnswers[0][0]) {
+  } else if (answer === incorrectAnswers[setId] || answer === incorrectAnswers[setId][0]) {
     // that is a NO.
-    alert('Sorry, that is not the right answer: he actually was born in the PacNW.');
+    alert('Sorry, that is the wrong answer.');
   } else {
     // the answer was crap!
     alert('Now, did you not read that answers could only be Yes, No, Y or N? Illiterate much? Or simply contrary?');
   }
   console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
 }
-birthQuestion();
 
-function passportQuestion() {
-  currentQuestion = theQuestions[1];
-  answer = prompt(currentQuestion).toLowerCase();
-  questionArray.push(currentQuestion);
-  answerArray.push(answer);
-
-  if (answer === correctAnswers[1] || answer === correctAnswers[1][0]) {
-    alert('Yeppers, John has a valid passport! He can leave the country. Hm...');
-    answerCount++;
-  } else if (answer === incorrectAnswers[1] || answer === incorrectAnswers[1][0]) {
-    alert('No, no, no! He does have a valid one!');
-  } else {
-    alert('Stop with the bad input, already!');
-  }
-  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
-}
-passportQuestion();
-
-function boneQuestion() {
-  currentQuestion = theQuestions[2];
-  answer = prompt(currentQuestion).toLowerCase();
-  questionArray.push(currentQuestion);
-  answerArray.push(answer);
-
-  if (answer === correctAnswers[2] || answer === correctAnswers[2][0]) {
-    alert('You answered YES, which is wrong-a-mundo!');
-  } else if (answer === incorrectAnswers[2] || answer === incorrectAnswers[2][0]) {
-    alert('No, John has been lucky enough to never have had a broken bone.');
-    answerCount++;
-  } else {
-    alert('What?! Crap input? Please!!');
-  }
-  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
-}
-boneQuestion();
-
-function viableQuestion() {
-  currentQuestion = theQuestions[3];
-  answer = prompt(currentQuestion).toLowerCase();
-  questionArray.push(currentQuestion);
-  answerArray.push(answer);
-
-  if (answer === correctAnswers[3] || answer === correctAnswers[3][0]) {
-    alert('Yeah, no. John does not have any off-spring. Unless those aliens have done something nefarious...');
-  } else if (answer === incorrectAnswers[3] || answer === incorrectAnswers[3][0]) {
-    alert('No children for John. He\'s enough of a child on his own. Correct!');
-    answerCount++;
-  } else {
-    alert('Bad input is just childish!');
-  }
-  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
-}
-viableQuestion();
-
-function dogQuestion() {
-  currentQuestion = theQuestions[4];
-  answer = prompt(currentQuestion).toLowerCase();
-  questionArray.push(currentQuestion);
-  answerArray.push(answer);
-
-  if (answer === correctAnswers[4] || answer === correctAnswers[4][0]) {
-    alert('Yes, he is a friend to dogs. At least friendly ones.');
-    answerCount++;
-  } else if (answer === incorrectAnswers[4] || answer === incorrectAnswers[4][0]) {
-    alert('Just what do you think of John?!!? He is shocked!');
-  } else {
-    alert('Invalid input is for the dogs.');
-  }
-  console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
-}
-dogQuestion();
-
+// function birthQuestion() {
+//   currentQuestion = theQuestions[0];
+//   answer = prompt(currentQuestion).toLowerCase();
+//   questionArray.push(currentQuestion);
+//   answerArray.push(answer);
+//
+//   if (answer === correctAnswers[0] || answer === correctAnswers[0][0]) {
+//     // it's a YES!
+//     alert('You got that right! He was born in Washington state.');
+//     answerCount++;
+//   } else if (answer === incorrectAnswers[0] || answer === incorrectAnswers[0][0]) {
+//     // that is a NO.
+//     alert('Sorry, that is not the right answer: he actually was born in the PacNW.');
+//   } else {
+//     // the answer was crap!
+//     alert('Now, did you not read that answers could only be Yes, No, Y or N? Illiterate much? Or simply contrary?');
+//   }
+//   console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+// }
+// birthQuestion();
+//
+// function passportQuestion() {
+//   currentQuestion = theQuestions[1];
+//   answer = prompt(currentQuestion).toLowerCase();
+//   questionArray.push(currentQuestion);
+//   answerArray.push(answer);
+//
+//   if (answer === correctAnswers[1] || answer === correctAnswers[1][0]) {
+//     alert('Yeppers, John has a valid passport! He can leave the country. Hm...');
+//     answerCount++;
+//   } else if (answer === incorrectAnswers[1] || answer === incorrectAnswers[1][0]) {
+//     alert('No, no, no! He does have a valid one!');
+//   } else {
+//     alert('Stop with the bad input, already!');
+//   }
+//   console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+// }
+// passportQuestion();
+//
+// function boneQuestion() {
+//   currentQuestion = theQuestions[2];
+//   answer = prompt(currentQuestion).toLowerCase();
+//   questionArray.push(currentQuestion);
+//   answerArray.push(answer);
+//
+//   if (answer === correctAnswers[2] || answer === correctAnswers[2][0]) {
+//     alert('You answered YES, which is wrong-a-mundo!');
+//   } else if (answer === incorrectAnswers[2] || answer === incorrectAnswers[2][0]) {
+//     alert('No, John has been lucky enough to never have had a broken bone.');
+//     answerCount++;
+//   } else {
+//     alert('What?! Crap input? Please!!');
+//   }
+//   console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+// }
+// boneQuestion();
+//
+// function viableQuestion() {
+//   currentQuestion = theQuestions[3];
+//   answer = prompt(currentQuestion).toLowerCase();
+//   questionArray.push(currentQuestion);
+//   answerArray.push(answer);
+//
+//   if (answer === correctAnswers[3] || answer === correctAnswers[3][0]) {
+//     alert('Yeah, no. John does not have any off-spring. Unless those aliens have done something nefarious...');
+//   } else if (answer === incorrectAnswers[3] || answer === incorrectAnswers[3][0]) {
+//     alert('No children for John. He\'s enough of a child on his own. Correct!');
+//     answerCount++;
+//   } else {
+//     alert('Bad input is just childish!');
+//   }
+//   console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+// }
+// viableQuestion();
+//
+// function dogQuestion() {
+//   currentQuestion = theQuestions[4];
+//   answer = prompt(currentQuestion).toLowerCase();
+//   questionArray.push(currentQuestion);
+//   answerArray.push(answer);
+//
+//   if (answer === correctAnswers[4] || answer === correctAnswers[4][0]) {
+//     alert('Yes, he is a friend to dogs. At least friendly ones.');
+//     answerCount++;
+//   } else if (answer === incorrectAnswers[4] || answer === incorrectAnswers[4][0]) {
+//     alert('Just what do you think of John?!!? He is shocked!');
+//   } else {
+//     alert('Invalid input is for the dogs.');
+//   }
+//   console.log('For the question:', currentQuestion, 'the answer supplied was:', answer);
+// }
+// dogQuestion();
+//
 console.log('There were ' + answerCount + ' correct answers, out of 5 total.');
 
 for (var i = 0; i < questionArray.length; i++) {
